deee1d2542118a82f5d31f6a66e06999aa90b8a1
gtjames <gjames@ancoraeducation.com>
2017-11-06 07:21:56 -0600

    include comments about Customer being added

--- src/main/java/edgetech/springframework/services/ProductService.java
+++ src/main/java/edgetech/springframework/services/ProductService.java
@@ -1,20 +1,51 @@
 package edgetech.springframework.services;
 
 import edgetech.springframework.domain.Product;
+import edgetech.springframework.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Iterator;
 
 /**
  * Created by Edge Tech Academy on 12/12/2016.
  */
-public interface ProductService {
-    Iterable<Product> listAllProducts();
+@Service
+public class ProductService {
 
-    Product getProductById(Integer id);
+	private ProductRepository productRepository;
+
+	@Autowired
+	public void setProductRepository(ProductRepository productRepository) {
+		this.productRepository = productRepository;
+	}
+
+	public Iterable<Product> listAllProducts() {
+		return productRepository.findAll();
+	}
+
+	public Product getProductById(Integer id) {
+		return productRepository.findOne(id);
+	}
 
 	//	These items have been added for the new functionality
 	//		to make our app a richer MVC example
-    Product saveProduct(Product product);
+	public Product saveProduct(Product product) {
+		return productRepository.save(product);
+	}
 
-    void deleteProduct(Integer id);
+	public void deleteProduct(Integer id) {
+		Product    product = productRepository.findOne(id);
+		productRepository.delete(product);
+	}
 
-    Iterable<Product> listOddProducts();
+	public Iterable<Product> listOddProducts() {
+		Iterable<Product> products = productRepository.findAll();
+		for(Iterator<Product> prodIterator = products.iterator(); prodIterator.hasNext();) {
+			Product prod = prodIterator.next();
+			if ((prod.getId() & 1) == 0)		// if the Id is Even
+				prodIterator.remove();			// remove from our list
+		}
+		return products;
+	}
 }
